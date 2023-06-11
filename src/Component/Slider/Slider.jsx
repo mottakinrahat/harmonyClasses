7a624200249a239b69f91206976c1c92a14c0c6d
mottakinrahat <rahatchowhdury661@gmail.com>
2023-06-12 05:03:45 +0600

    sliderAdded

--- src/Component/Slider/Slider.jsx
+++ src/Component/Slider/Slider.jsx
@@ -2,24 +2,37 @@ import React from 'react';
 import slider_1 from '../../assets/Sliders/slider-1.jpg'
 import slider_2 from '../../assets/Sliders/slider-2.jpg'
 import slider_3 from '../../assets/Sliders/slider-3.jpg'
-import Title from '../Title/Title';
+import guitar_1 from '../../assets/Sliders/guitar-1.jpg'
+import guitar_2 from '../../assets/Sliders/guitar-2.jpg'
+import guitar_3 from '../../assets/Sliders/guitar-3.jpg'
 const Slider = () => {
     return (
         <div>
-            <div className="carousel w-[1200px] h-[600px] md:mx-[150px] ">
+            <div className="carousel w-full h-[700px] md:mx-auto">
                 <div id="slide1" className="carousel-item relative w-full ">
-                    <img src={slider_1} className="w-full" />
+                    <img src={slider_3} className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide4" className="btn btn-circle">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
 
                     </div>
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-2xl font-bold">
-                        Text Overlay for Slide 1
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-2xl font-bold w-full px-8 ">
+                        <div className='flex justify-between items-center gap-10'>
+                            <div className='px-8 w-1/2'>
+                                <h1 className='text-6xl mb-10'><span className='text-yellow-400'>Unlock Your Musical Talent with</span> Hermonia</h1>
+                                <p className='text-[15px] mb-10'>Embark on a harmonious journey of musical discovery at our esteemed music school. Unleash your creativity, refine your skills, and ignite your passion for music under the guidance of our expert instructors. From classical to contemporary, we offer a comprehensive curriculum tailored to nurture your unique talents. Join our vibrant community and experience the transformative power of music today!</p>
+                                <button className='btn btn-warning'>See more</button>
+                            </div>
+                            <div className='flex border-2 rounded w-1/2 mr-10'>
+                                <img src={guitar_1} className='h-80 w-[700px] ' alt="" />
+                                <img src={guitar_2} className='h-80 w-[700px] ' alt="" />
+                                <img src={guitar_3} className='h-80 w-[700px] ' alt="" />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <img src={slider_2} />
+                    <img src={slider_2} className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
                         <a href="#slide3" className="btn btn-circle">❯</a>
@@ -29,7 +42,7 @@ const Slider = () => {
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <img src={slider_3} />
+                    <img src={slider_1} className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide2" className="btn btn-circle">❮</a>
                         <a href="#slide4" className="btn btn-circle">❯</a>
@@ -39,7 +52,7 @@ const Slider = () => {
                     </div>
                 </div>
             </div>
-            
+
         </div>
     );
 };
